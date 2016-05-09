b8f49cdd128a9ee91e3e30575f243b445ed4422c
David Sweeney <sweeney@tamu.edu>
2016-05-08 23:28:38 -0500

    Re-adds and re-styles the error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Down for Maintenance</title>
+        <title>Restricted</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <style>
             html, body {
                 height: 100%;
@@ -40,7 +40,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Uh oh, <br>looks like we're down for maintenance.  <br>Be back soon.</div>
+                <div class="title">Sorry, Admins Only</div>
+                <button type="button" class="btn btn-primary" onClick="location='/home'" name="button">Back to Safety</button>
             </div>
         </div>
     </body>
