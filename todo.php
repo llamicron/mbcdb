715948290bfac535875834776c93e962193bb03d
Luke Sweeney <luke@thesweeneys.org>
2016-08-17 12:51:14 -0500

    test for master branch

--- todo.php
+++ todo.php
@@ -1,4 +1,2 @@
 <?php
-// FIXME: Sorting by troop, council, district, and name destroy search results
 // TODO: Finish email verification
-// TODO: add minimum password length for users. maybe already done?
