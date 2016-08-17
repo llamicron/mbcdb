908c0f9b4bab6d9a417805ecc65b838ed4da7252
Luke Sweeney <luke@thesweeneys.org>
2016-08-17 12:46:50 -0500

    Test for git branch

--- todo.php
+++ todo.php
@@ -1,4 +1,3 @@
 <?php
-// FIXME: Sorting by troop, council, district, and name destroy search results
 // TODO: Finish email verification
-// TODO: add minimum password length for users. maybe already done?
+// XXX: Test for another branch
