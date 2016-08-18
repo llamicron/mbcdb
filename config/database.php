5c69b9c39d59a6b6692ce5c0774a0aa38e4e0618
Luke Sweeney <luke@thesweeneys.org>
2016-08-18 00:07:32 -0500

    commit to deploy to aws server

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!defined('RDS_HOSTNAME')) {
+  define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+  define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+  define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+  define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+}
+
 return [
 
     /*
@@ -54,11 +61,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mbcdb'),
-            'username' => env('DB_USERNAME', 'mbcdb'),
-            'password' => env('DB_PASSWORD', 'meritbadges123'),
+            'database' => RDS_DB_NAME,
+            'username' => RDS_USERNAME,
+            'password' => RDS_PASSWORD,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
