c568ed200720dbd958366dd592a3f0f664b3bd09
Luke Sweeney <luke@thesweeneys.org>
2016-08-18 01:30:08 -0500

    Changes database.php to production settings

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 // keep commented for localhost.  this file is .gitignored, so you shouldn't have to worry about this.
 // production server should be uncommented
-// if (!defined('RDS_HOSTNAME')) {
-//   define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-//   define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-//   define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-//   define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-// }
+ if (!defined('RDS_HOSTNAME')) {
+   define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+   define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+   define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+   define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+ }
 
 return [
 
@@ -62,11 +62,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST'),
+            'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'database' => RDS_DB_NAME,
+            'username' => RDS_USERNAME,
+            'password' => RDS_PASSWORD,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
