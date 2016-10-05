bb9224625e3566fd372046e5a579fef7ad696764
Luke Sweeney <luke@thesweeneys.org>
2016-10-05 11:21:25 -0500

    Sections Seeders into Dev and Prod groups. WARNING: .env CHANGES

--- app/Badge.php
+++ app/Badge.php
@@ -4,10 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Badge extends Model
-{
-     public function counselors()
-     {
+class Badge extends Model {
+     public function counselors() {
        return $this->belongsToMany("App\Counselor");
      }
 }
